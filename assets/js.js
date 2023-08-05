ac38d2be4ac11e4238fa7e933367145895c1a64d
Ahmedalla1144 <107187857+Ahmedalla1144@users.noreply.github.com>
2023-08-05 17:07:40 +0300

    go

--- assets/js.js
+++ assets/js.js
@@ -12,29 +12,33 @@ const show_products = function ({
   title,
 }) {
   container.innerHTML += `
-      <div class="details">
-        
-        <img src="${thumbnail}" alt="${id}" />
-        
-        <h4>Title: <span>${title}</span></h4>
-        
-        <p>Brand: <span>${brand}</span></p>
-        
-        <p>Category: <span>${category}</span></p>
-        
-        <p>Description:<span>${description}</span></p>
-        
-        <p>DiscountPercentage: <span>${discountPercentage}</span></p>
-        
-        <p>
-          Price:<span> ${price}</span> Rating:<span> ${rating} </span>Stock:<span>
-            ${stock}</span>
-        </p>
-
-        <div class="button">
-          <button>Buy now</button>
+    <div class="details">
+        <div>
+            <img src="${thumbnail}" alt="${id}" />
         </div>
-      </div>
+  
+        <div class="detail">
+            <h4>Title: <span>${title}</span></h4>
+        
+            <p>Brand: <span>${brand}</span></p>
+        
+            <p>Category: <span>${category}</span></p>
+        
+            <p>Description:<span>${description}</span></p>
+        
+            <p>DiscountPercentage: <span>${discountPercentage}</span></p>
+        
+            <p>
+                Price:<span> ${price}</span> Rating:<span> ${rating} </span>Stock:<span>
+                ${stock}</span>
+            </p>
+
+        </div>    
+        <div class="button">
+            <button>Buy now</button>
+        </div>
+    </div>
+    
       `;
 };
 
@@ -51,3 +55,16 @@ const get_products = async function () {
 };
 
 get_products();
+
+const get_clock = function () {
+  const now = new Date();
+  const scnds = now.getSeconds();
+  const mins = now.getMinutes();
+  const hours = now.getHours();
+
+  scnd.innerHTML = scnds;
+  min.innerHTML = mins;
+  hour.innerHTML = hours;
+};
+get_clock();
+setInterval(get_clock, 1000);
